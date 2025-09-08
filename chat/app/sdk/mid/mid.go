3a36e75f0ac0ab0e44b0ea0b02277ef6f3b207f4
kcharymyrat <kcharymyrat@gmail.com>
2025-09-08 18:50:06 +0500

    app sdk packages

---
+++ chat/app/sdk/mid/mid.go
@@ -0,0 +1,17 @@
+// Package mid provides app level middleware support.
+package mid
+
+import (
+	"github.com/kcharymyrat/usdl/chat/foundation/web"
+)
+
+// =============================================================================
+
+// isError tests if the Encoder has an error inside of it.
+func isError(e web.Encoder) error {
+	err, isError := e.(error)
+	if isError {
+		return err
+	}
+	return nil
+}
